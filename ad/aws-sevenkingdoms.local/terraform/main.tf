c4b0853f184ca0d5b04dd3c80fd7567c9c01f33a
“ademenet” <alain.demenet@gmail.com>
2023-10-18 12:49:27 +0200

    update: add IP public output

--- ad/aws-sevenkingdoms.local/terraform/main.tf
+++ ad/aws-sevenkingdoms.local/terraform/main.tf
@@ -12,38 +12,37 @@ resource "aws_key_pair" "terraformkey" {
 }
 
 # Our VPC definition, using a default IP range of 192.168.0.0/16
-resource "aws_vpc" "lab-vpc" {
+resource "aws_vpc" "goad_vpc" {
   cidr_block           = var.VPC_CIDR
   enable_dns_support   = true
   enable_dns_hostnames = true
 }
 
 # Default route required for the VPC to push traffic via gateway
-resource "aws_route" "lab-internet-route" {
-  route_table_id         = aws_vpc.lab-vpc.main_route_table_id
+resource "aws_route" "goad_internet_route" {
+  route_table_id         = aws_vpc.goad_vpc.main_route_table_id
   destination_cidr_block = "0.0.0.0/0"
-  gateway_id             = aws_internet_gateway.lab-vpc-gateway.id
+  gateway_id             = aws_internet_gateway.goad_vpc_gateway.id
 }
 
 # Gateway which allows outbound and inbound internet access to the VPC
-resource "aws_internet_gateway" "lab-vpc-gateway" {
-  vpc_id = aws_vpc.lab-vpc.id
+resource "aws_internet_gateway" "goad_vpc_gateway" {
+  vpc_id = aws_vpc.goad_vpc.id
 }
 
 # Create our first subnet (Defaults to 192.168.56.0/24)
-resource "aws_subnet" "lab-vpc-subnet-1" {
-  vpc_id = aws_vpc.lab-vpc.id
-
+resource "aws_subnet" "goad_vpc_subnet_private" {
+  vpc_id            = aws_vpc.goad_vpc.id
   cidr_block        = var.SUBNET1_CIDR
   availability_zone = "${var.REGION}a"
 
   tags = {
-    Name = "GOAD Subnet 1"
+    Name = "GOAD Subnet private"
   }
 }
 
 # Set DHCP options for delivering things like DNS servers
-resource "aws_vpc_dhcp_options" "lab-dhcp" {
+resource "aws_vpc_dhcp_options" "goad_dhcp" {
   domain_name          = "sevenkingdoms.local"
   domain_name_servers  = [var.DC01_IP, var.PUBLIC_DNS]
   ntp_servers          = [var.DC01_IP]
@@ -56,42 +55,42 @@ resource "aws_vpc_dhcp_options" "lab-dhcp" {
 }
 
 # Associate our DHCP configuration with our VPC
-resource "aws_vpc_dhcp_options_association" "lab-dhcp-assoc" {
-  vpc_id          = aws_vpc.lab-vpc.id
-  dhcp_options_id = aws_vpc_dhcp_options.lab-dhcp.id
+resource "aws_vpc_dhcp_options_association" "goad_dhcp_assoc" {
+  vpc_id          = aws_vpc.goad_vpc.id
+  dhcp_options_id = aws_vpc_dhcp_options.goad_dhcp.id
 }
 
-# output "dc01" {
-#   value       = aws_instance.dc01.public_ip
-#   description = "Public IP of DC01"
-# }
+output "dc01" {
+  value       = aws_instance.dc01.public_ip
+  description = "Public IP of DC01"
+}
 
-# output "dc02" {
-#   value       = aws_instance.dc02.public_ip
-#   description = "Public IP of DC02"
-# }
+output "dc02" {
+  value       = aws_instance.dc02.public_ip
+  description = "Public IP of DC02"
+}
 
-# output "srv02" {
-#   value       = aws_instance.srv02.public_ip
-#   description = "Public IP of SRV02"
-# }
+output "srv02" {
+  value       = aws_instance.srv02.public_ip
+  description = "Public IP of SRV02"
+}
 
-# output "dc03" {
-#   value       = aws_instance.dc03.public_ip
-#   description = "Public IP of DC03"
-# }
+output "dc03" {
+  value       = aws_instance.dc03.public_ip
+  description = "Public IP of DC03"
+}
 
-# output "srv03" {
-#   value       = aws_instance.srv03.public_ip
-#   description = "Public IP of SRV03"
-# }
+output "srv03" {
+  value       = aws_instance.srv03.public_ip
+  description = "Public IP of SRV03"
+}
 
 output "wg" {
   value       = aws_eip.wg_eip.public_ip
   description = "Public IP of WG"
 }
 
-output "admin" {
-  value       = aws_instance.admin.public_ip
-  description = "Public IP of Admin"
-}
+# output "admin" {
+#   value       = aws_instance.admin.public_ip
+#   description = "Public IP of Admin"
+# }
